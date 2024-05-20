ee784fe566d3de5c9fc515db79ba0295b201af6f
rubendewindt <ruben-dw@hotmail.be>
2024-05-20 21:05:09 +0200

    service worker draaiende gemaakt (cache en offline fallback) en button voor navigatie toegevoegd

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,23 @@
+// Bekijk of de pagina geladen is. Zoja, registreer de service worker.
 window.addEventListener("load", function(){
+    console.log("Service worker registration started...");
+
+    // Kunnen we met een SW werken in deze browser?
     if("serviceWorker" in this.navigator)
     {
-        this.navigator.serviceWorker.register("service-worker.js")
-        .then((registration) => {
-            console.log("Registered: ", registration);
-        })
+        // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerContainer/register
+        this.navigator.serviceWorker.register("scripts/service-worker.js")
+        .then((registration) => console.log("Registered: ", registration))
         .catch((error) => console.log("Error: ", error));
     }
     else
-        console.log("No service worker support in this browser.");
+        console.log("No service worker supported...");
+
+
+        //navigeer button 
+
+        document.getElementById('NaarQuizButton').addEventListener('click', function() {
+            window.location.href = 'offline.html';
+        });
 });
+
--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,11 +1,69 @@
+const STATIC_CACHE_NAME = "static-version-1";
+const DYNAMIC_CACHE_NAME = "dynamic-version-1";
+
+// Array met alle static files die gecached moeten worden.
+const staticFiles = [
+    '/index.html',
+    '/offline.html',
+    '/scripts/app.js',
+    '/scripts/service-worker.js',
+    '/styles/style.css',
+    '/manifest.json',
+    '/images/icons/favicon.ico',
+    '/images/icons/vives48.png',
+    '/images/icons/vives72.png',
+    '/images/icons/vives96.png',
+    '/images/icons/vives144.png',
+    '/images/icons/vives168.png',
+    '/images/icons/vives192.png',
+    '/images/icons/vives512.png'
+];
+
 self.addEventListener("install", (event) => {
     console.log("Service worker installed: ", event);
+
+    // De eerste keer dat de app opstart, de cache vullen.
+    //wachten tot alle promises voldaan zijn
+    //dus alle code hieronder
+    event.waitUntil(
+        caches.open(STATIC_CACHE_NAME).then(cache => {
+            console.log("Caching static files...");
+            //verwacht een itreble bv een array
+            cache.addAll(staticFiles);
+        })
+    )
 });
 
 self.addEventListener("activate", (event) => {
     console.log("Service worker activated: ", event);
+
+    // Bij een nieuwe versie van de Service Worker, eventueel de oude cache wissen.
+    event.waitUntil(
+        caches.keys().then(keys => {
+            console.log("Cache keys: ", keys);
+
+            return Promise.all(keys
+                .filter(key => ((key !== STATIC_CACHE_NAME) && (key !== DYNAMIC_CACHE_NAME)))
+                .map(key => caches.delete(key))
+                )
+        })
+    );
 });
 
 self.addEventListener("fetch", (event) => {
     console.log("Fetch-event: ", event);
+
+    event.respondWith(
+        caches.match(event.request).then(cacheResponse => {
+            return cacheResponse || fetch(event.request).then(fetchResponse => {
+                return caches.open(DYNAMIC_CACHE_NAME).then(cache => {
+                    cache.put(event.request.url, fetchResponse.clone());
+                    return fetchResponse;
+                });
+            }).catch(() => {
+                // If fetch fails (e.g., when offline), return the offline page
+                return caches.match('/offline.html');
+            });
+        })
+    );
 });
